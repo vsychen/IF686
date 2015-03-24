ebcfe81d4fc9a11ba3a28d638ad0cea9f4020b05
Victor Chen <vsyc@cin.ufpe.br>
2015-03-24 12:21:18 -0300

    Alteração de formato
    Extração do arquivo .zip da aula 03 para .hs, para melhor leitura do
    mesmo.
    Adição de parte dos exercícios. O resto dos exercícios será adicionado
    nos próximos commits

---
+++ 20150324.hs
@@ -0,0 +1,133 @@
+{-	Universidade Federal de Pernambuco
+	Centro de Informática (CIn)
+	Alunos: Victor Sin Yu Chen & Lucas de Souza Albuquerque
+	Login: vsyc & lsa2
+-}
+
+-- Projeto
+-- mergesort
+mergesort :: [Int] -> [Int]
+mergesort a = if (length a) <= 1
+              then a
+              else merge (mergesort (take (div (length a) 2) a)) (mergesort (drop (div (length a) 2) a))
+
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if (x <= y)
+            then x : merge xs (y:ys)
+			else y : merge (x:xs) ys
+
+-- heapsort
+swap :: Int -> Int -> [Int] -> [Int]
+swap i j xs | i == j    = xs
+            | i < j     = (take i xs) ++ ((xs!!j):[]) ++ (drop (i+1) (take j xs)) ++ ((xs!!i):[]) ++ (drop (j+1) xs)
+            | otherwise = (take j xs) ++ ((xs!!i):[]) ++ (drop (j+1) (take i xs)) ++ ((xs!!j):[]) ++ (drop (i+1) xs)
+
+large :: Int -> Int -> [Int] -> Int
+large i l xs = if ((2*i+1) < l) && ((xs!!(2*i+1)) > (xs!!i))
+               then (2*i+1)
+               else i
+
+largest :: Int -> Int -> [Int] -> Int
+largest i l xs = if ((2*i+2) < l) && ((xs!!(2*i+2)) > (xs!!(large i l xs)))
+                 then (2*i+2)
+                 else large i l xs
+
+heapify :: Int -> Int -> [Int] -> [Int]
+heapify i l xs = if ((largest i l xs) /= i)
+                 then heapify (largest i l xs) l (swap (largest i l xs) i xs)
+                 else xs
+
+buildHeap :: Int -> [Int] -> [Int]
+buildHeap i xs | i <= 0    = heapify 0 (length xs) xs
+               | otherwise = buildHeap (i-1) (heapify i (length xs) xs)
+
+hpsort :: Int -> [Int] -> [Int]
+hpsort i xs = if (i == 1)
+              then heapify 0 i (swap 0 i xs)
+              else hpsort (i-1) (heapify 0 i (swap 0 i xs))
+
+heapsort :: [Int] -> [Int]
+heapsort xs | (length xs) <= 1 = xs
+heapsort xs = hpsort ((length xs)-1) (buildHeap (div (length xs) 2) xs)
+
+-- Exercícios
+-- menorMaior
+menorMaior :: Int -> Int -> Int -> (Int, Int)
+menorMaior x y z | ((x > y) && (x < z)) || ((x < y) && (x > z)) = (y, z)
+                 | ((y > x) && (y < z)) || ((y < x) && (y > z)) = (x, z)
+                 | ((z > x) && (z < y)) || ((z < x) && (z > y)) = (x, y)
+                 | otherwise = (-1, -1)
+
+-- ordenaTripla (ordenacao MIN->MAX)
+ordenaTripla :: (Int, Int, Int) -> (Int, Int, Int)
+ordenaTripla (x, y, z) | (x >= y && y >= z) = (z, y, x)
+                       | (x >= z && z >= y) = (y, z, x)
+                       | (y >= x && x >= z) = (z, x, y)
+                       | (y >= z && z >= x) = (x, z, y)
+                       | (z >= x && x >= y) = (y, x, z)
+                       | (z >= y && y >= x) = (x, y, z)
+
+-- getX & getY
+type Ponto = (Float, Float)
+
+getX :: Ponto -> Float
+getX p = (fst p)
+
+getY :: Ponto -> Float
+getY p = (snd p)
+
+-- vertical
+type Reta = (Ponto, Ponto)
+
+vertical :: Reta -> Bool
+vertical r = if (snd (fst r) == snd (snd r))
+             then True
+             else False
+
+-- pontoY sera igual a 0
+
+type Pessoa = String
+type Livro = String
+type BancoDados = [(Pessoa, Livro)]
+
+baseExemplo :: BancoDados
+baseExemplo = [("Sergio","O Senhor dos Aneis"),("Andre","Duna"),("Fernando","Jonathan Strange & Mr. Norrell"), ("Fernando","A Game of Thrones")]
+
+-- livros
+livros :: BancoDados -> Pessoa -> [Livro]
+livros [] _ = []
+livros ((p,l):xs) y | y == p    = l:livros xs y
+                    | otherwise = livros xs y
+
+-- emprestimos
+emprestimos :: BancoDados -> Livro -> [Pessoa]
+emprestimos [] _ = []
+emprestimos ((p,l):xs) y | y == l    = p:emprestimos xs y
+                         | otherwise = emprestimos xs y
+
+-- emprestado
+emprestado :: BancoDados -> Livro -> Bool
+emprestado [] _ = False
+emprestado ((p,l):xs) y | y == l    = True
+                        | otherwise = emprestado xs y
+
+-- qtdEmprestimos
+qtdEmprestimos :: BancoDados -> Pessoa -> Int
+qtdEmprestimos [] _ = 0
+qtdEmprestimos ((p,l):xs) y | y == p    = 1+qtdEmprestimos xs y
+                            | otherwise = qtdEmprestimos xs y
+
+-- emprestar
+emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar x p l = (p,l):x
+
+-- devolver
+devolver :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver x p l | x == []                                      = []
+               | (p == (fst (head x)) && l == (snd (head x))) = devolver (tail x) p l
+               | otherwise                                    = (head x):(devolver (tail x) p l)
+
+-- membro
+membro :: [Int] -> Int -> Bool
