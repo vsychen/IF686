a085f29761a3e2e3e6676a6363b31ce7fbf8537c
Victor Chen <vsyc@cin.ufpe.br>
2015-04-07 12:56:20 -0300

    Aula 06

---
+++ 20150407.hs
@@ -0,0 +1,61 @@
+-- area
+data Shape = Circle Float
+           | Rectangle Float Float
+
+area :: Shape -> Float
+area (Circle x) = 2*pi*(x)^2
+area (Rectangle x y) = x*y
+
+-- isWeekend
+type HorasAula = Int
+type Discip = [String]
+data WeekDay = Segunda HorasAula Discip
+             | Terca HorasAula Discip
+             | Quarta HorasAula Discip
+             | Quinta HorasAula Discip
+             | Sexta HorasAula Discip
+             | Sabado HorasAula Discip
+             | Domingo
+
+isWeekend :: WeekDay -> Bool
+isWeekend (Sabado _ _) = True
+isWeekend (Domingo) = True
+isWeekend _ = False
+
+-- hasPLC
+temMateria :: Discip -> String -> Bool
+temMateria x mat = if x == [] then False
+                   else if (head x) == mat then True
+                   else temMateria (tail x) mat
+
+hasPLC :: WeekDay -> Bool
+hasPLC (Domingo) = False
+hasPLC (Segunda h disc) = temMateria disc "PLC"
+hasPLC (Terca h disc) = temMateria disc "PLC"
+hasPLC (Quarta h disc) = temMateria disc "PLC"
+hasPLC (Quinta h disc) = temMateria disc "PLC"
+hasPLC (Sexta h disc) = temMateria disc "PLC"
+hasPLC (Sabado h disc) = temMateria disc "PLC"
+
+-- tree
+data Tree t = NilT | Node t (Tree t) (Tree t) deriving (Eq, Show)
+
+-- showExpr
+data Expr = Lit Int | Add Expr Expr | Sub Expr Expr
+
+showExpr :: Expr -> String
+showExpr (Lit n) = show n
+showExpr (Add e1 e2) = "(" ++ (showExpr e1) ++ "+" ++ (showExpr e2) ++ ")"
+showExpr (Sub e1 e2) = "(" ++ (showExpr e1) ++ "-" ++ (showExpr e2) ++ ")"
+
+-- toList
+data List t = Nil | Cons t (List t)
+
+toList :: List t -> [t]
+toList (Nil) = []
+toList (Cons x (xs)) = x : toList xs
+
+-- fromList **atualizar
+fromList :: [t] -> List t
+fromList [] = "Nil"
+fromList (x:xs) = "Cons" ++ (show x) ++ (fromList xs)
