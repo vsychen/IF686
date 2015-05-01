d6fa970ee8dcd150f161acfa46cda1545d55ac1c
vsychen <vsyc@cin.ufpe.br>
2015-05-01 16:30:59 -0300

    Atualização Aula 30/04/2015
    Atualização do exercício

--- 20150430.hs
+++ 20150430.hs
@@ -1,13 +1,36 @@
 import Control.Monad
 
-data Failable t = Error String | Value t
+data Failable t = Error String | Value t deriving (Show)
 
 instance Monad Failable where
  (>>=) (Error x) _ = Error x
  (>>=) (Value x) f = f x
  return x = (Value x)
 
-data Fila t = NilF | Node t (Fila t)
+data Fila t = NilF | Node t (Fila t) deriving (Show)
 
+-- criarFila
 criarFila :: Int -> t -> Failable (t, Fila t)
-criarFila l f = 
+criarFila x f = if x <= 0 then Error "Tamanho incompativel"
+                else Value (f, (Node f (NilF)))
+
+-- push
+isFull :: Fila t -> Int -> Bool
+isFull (NilF) l = if l < 0 then True
+                  else False
+isFull (Node x f) l = isFull f (l - 1)
+
+push :: Int -> t -> Fila t -> Failable (t, Fila t)
+push l x (NilF) = criarFila l x
+push l x f = if (isFull f (l - 1)) then Error "Lista cheia"
+             else return (x, (Node x f))
+
+-- pop
+pop :: Fila t -> Failable (t, Fila t)
+pop (NilF) = Error "Lista vazia"
+pop (Node x f) = return (x, f)
+
+-- peek (o que é que PEEK faz? '-')
+peek :: Fila t -> Failable (t, Fila t)
+peek (NilF) = Error "Lista vazia"
+peek (Node x f) = return (x, (Node x f))
