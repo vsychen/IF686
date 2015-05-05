eb0a352d1d177eb56cc9205b8ac161991993607b
Victor Chen <vsyc@cin.ufpe.br>
2015-05-05 08:08:40 -0300

    Trabalho 11
    Sem a questão 1. KD a taBla

--- 20150505.hs
+++ 20150505.hs
@@ -9,6 +9,35 @@
 {-Trabalho 11, Questão 2-}
 import Control.Monad
 
+-- métodos auxiliares
+-- getSpace
+getSpace :: String -> Int
+getSpace x | x == [] = 0
+           | (head x) == ' ' = 0
+           | otherwise = 1 + getSpace (tail x)
+
+-- getWord
+getWord :: String -> String
+getWord x | x == [] = []
+          | otherwise = take (getSpace x) x
+
+-- dropWord
+dropWord :: String -> String
+dropWord x | x == [] = []
+           | otherwise = drop (getSpace x) x
+
+-- dropSpace
+dropSpace :: String -> String
+dropSpace x | x == [] = []
+            | x!!0 /= ' ' = x
+            | otherwise = dropSpace (tail x)
+
+-- splitWords
+splitWords :: String -> [String]
+splitWords x | x == [] = []
+             | ((getSpace x) == 0 && (x!!0 /= ' ')) = (getWord x):[]
+             | otherwise = (getWord x) : (splitWords (dropSpace (dropWord x)))
+
 strCheck :: String -> [Maybe Char]
 strCheck [] = [Nothing]
 strCheck (s:ss) = if ((fromEnum s) > 64 && (fromEnum s) < 91) || ((fromEnum s) > 96 && (fromEnum s) < 123) then (Just s) : (strCheck ss)
@@ -20,15 +49,17 @@ strCaps ((Nothing):ss) = (Nothing) : (strCaps ss)
 strCaps ((Just s):ss) = if (fromEnum s) > 96 then (Just (toEnum ((fromEnum s) - 32))) : (strCaps ss)
                         else (Just s) : (strCaps ss)
 
-strPart :: [Maybe Char] -> [String]
+strPart :: [Maybe Char] -> String
 strPart [] = []
-strPart ((Nothing):ss) = [] : (strPart ss)
-strPart ((Just s):ss) = [[s]] ++ (strPart ss)
-{-
+strPart ((Nothing):ss) = ' ' : (strPart ss)
+strPart ((Just s):ss) = s : (strPart ss)
+
 main = do {
- input <- getLine; -- strRead
- check = strCheck input; -- strCheck
- caps = strCaps check; -- strCaps
- part = strPart caps; -- strPart
- putStrLn (part) -- strPrint
-}-}
+ input <- getLine;
+ mapM_ putStrLn (splitWords $ strPart $ strCaps $ strCheck input);
+ main
+}
+
+{- <<path>> --make 20150505.hs
+   20150505.exe
+-}
